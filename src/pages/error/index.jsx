beec75830ad84915710d1d4a74600d6392edf498
mathesukkj <kemuel.g7363@gmail.com>
2022-11-12 13:05:36 -0300

    Added link to Home on 404 page

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./error.css";
 
 export default function Error() {
@@ -5,6 +6,10 @@ export default function Error() {
         <div className="error">
             <h1>Página não encontrada!</h1>
             <p>Essa página não existe.</p>
+
+            <Link className="link" to="/">
+                Voltar pra home
+            </Link>
         </div>
     );
 }
