778b1d09472bf5192b530dc03659bb11c7a2632c
mathesukkj <kemuel.g7363@gmail.com>
2022-11-12 15:45:32 -0300

    Added useState to login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,30 @@
+import "./login.css";
+import { Logo } from "../../components/Logo";
+import { useState } from "react";
+
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div>
-            <h1>Página de Login</h1>
+        <div className="login-container">
+            <Logo />
+            <form className="form">
+                <input
+                    type="email"
+                    placeholder="Digite seu email..."
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="***********"
+                    autoComplete="on"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Acessar</button>
+            </form>
         </div>
     );
 }
