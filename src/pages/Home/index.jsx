459c09519f66aa823e6fea51bc39fa72870e4eb9
mathesukkj <kemuel.g7363@gmail.com>
2022-11-12 13:32:35 -0300

    Added Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,30 @@
+import "./home.css";
+
 export default function Home() {
     return (
-        <div>
-            <h1>Página Home</h1>
+        <div className="container">
+            <h1>Mathesu</h1>
+            <span>Veja meus links</span>
+
+            <main className="links">
+                <section className="link-area">
+                    <a href="#">
+                        <p className="link-text">NLW eSports</p>
+                    </a>
+                </section>
+
+                <section className="link-area">
+                    <a href="#">
+                        <p className="link-text">RocketPay</p>
+                    </a>
+                </section>
+
+                <section className="link-area">
+                    <a href="#">
+                        <p className="link-text">GitHub</p>
+                    </a>
+                </section>
+            </main>
         </div>
     );
 }
