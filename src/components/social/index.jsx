b877d270f0bc7cba519f21e2d43d025839c0f574
mathesukkj <kemuel.g7363@gmail.com>
2022-11-12 15:25:27 -0300

    Created SocialMedia component

---
+++ src/components/social/index.jsx
@@ -0,0 +1,14 @@
+import "./social.css";
+
+export function Social({ children, url }) {
+    return (
+        <a
+            className="social"
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            {children}
+        </a>
+    );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
 import "./home.css";
+import { Social } from "../../components/social";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 export default function Home() {
     return (
@@ -24,6 +26,18 @@ export default function Home() {
                         <p className="link-text">GitHub</p>
                     </a>
                 </section>
+
+                <footer>
+                    <Social url="https://www.linkedin.com/in/matheus-kemuel/">
+                        <FaLinkedin size={30} color="#FFF" />
+                    </Social>
+                    <Social url="https://github.com/mathesukkj">
+                        <FaGithub size={30} color="#FFF" />
+                    </Social>
+                    <Social url="https://www.instagram.com/mathesukkkkj/">
+                        <FaInstagram size={30} color="#FFF" />
+                    </Social>
+                </footer>
             </main>
         </div>
     );
