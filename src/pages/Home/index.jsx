9ab6162d3e56b1a049f4a1086985bf632a21fa7a
mathesukkj <kemuel.g7363@gmail.com>
2022-11-13 19:47:09 -0300

    Added home links

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,58 @@
 import "./home.css";
 import { Social } from "../../components/Social";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import { getDocs, collection, orderBy, query } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
 
 export default function Home() {
+    const [links, setLinks] = useState([]);
+
+    useEffect(() => {
+        function loadLinks() {
+            const linksRef = collection(db, "links");
+            const queryRef = query(linksRef, orderBy("created", "asc"));
+
+            getDocs(queryRef).then((snapshot) => {
+                let lista = [];
+
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        url: doc.data().url,
+                        name: doc.data().name,
+                        bg: doc.data().bg,
+                        color: doc.data().color,
+                    });
+                });
+                setLinks(lista);
+            });
+        }
+
+        loadLinks();
+    }, []);
+
     return (
         <div className="container">
             <h1>Mathesu</h1>
-            <span>Veja meus links</span>
-
+            <span>Veja meus projetos</span>
             <main className="links">
-                <section className="link-area">
-                    <a href="#">
-                        <p className="link-text">NLW eSports</p>
-                    </a>
-                </section>
-
-                <section className="link-area">
-                    <a href="#">
-                        <p className="link-text">RocketPay</p>
-                    </a>
-                </section>
-
-                <section className="link-area">
-                    <a href="#">
-                        <p className="link-text">GitHub</p>
-                    </a>
-                </section>
+                {links.map((item) => (
+                    <section
+                        key={item.id}
+                        className="link-area"
+                        style={{ backgroundColor: item.bg }}
+                    >
+                        <a href={item.url}>
+                            <p
+                                className="link-text"
+                                style={{ color: item.color }}
+                            >
+                                {item.name}
+                            </p>
+                        </a>
+                    </section>
+                ))}
 
                 <footer>
                     <Social url="https://www.linkedin.com/in/matheus-kemuel/">
